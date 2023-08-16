65c4d62b5737c256a69048f7ede1017e6a61f3eb
egolikov <panda_93a@mail.ru>
2023-08-17 00:08:33 +0600

    * upd Files tests

--- src/test/java/guru/qa/JsonTest.java
+++ src/test/java/guru/qa/JsonTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.io.InputStream;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.assertj.core.api.Assertions;
 
 public class JsonTest {
 
@@ -23,28 +23,28 @@ public class JsonTest {
             ObjectMapper objectMapper = new ObjectMapper();
             List<PersonModel> personModel = objectMapper.readValue(stream, new TypeReference<>() {});
 
-            assertEquals(3, personModel.size());
+            Assertions.assertThat(personModel).hasSize(3);
 
             PersonModel firstPerson = personModel.get(0);
-            assertEquals(1, firstPerson.getId());
-            assertEquals("Evgeniy", firstPerson.getFirstName());
-            assertEquals("Golikov", firstPerson.getLastName());
-            assertEquals("Russia", firstPerson.getCountry());
-            assertEquals(29, firstPerson.getAge());
+            Assertions.assertThat(firstPerson.getId()).isEqualTo(1);
+            Assertions.assertThat(firstPerson.getFirstName()).isEqualTo("Evgeniy");
+            Assertions.assertThat(firstPerson.getLastName()).isEqualTo("Golikov");
+            Assertions.assertThat(firstPerson.getCountry()).isEqualTo("Russia");
+            Assertions.assertThat(firstPerson.getAge()).isEqualTo(29);
 
             PersonModel secondPerson = personModel.get(1);
-            assertEquals(2, secondPerson.getId());
-            assertEquals("Alexandra", secondPerson.getFirstName());
-            assertEquals("Golikova", secondPerson.getLastName());
-            assertEquals("Russia", secondPerson.getCountry());
-            assertEquals(28, secondPerson.getAge());
+            Assertions.assertThat(secondPerson.getId()).isEqualTo(2);
+            Assertions.assertThat(secondPerson.getFirstName()).isEqualTo("Alexandra");
+            Assertions.assertThat(secondPerson.getLastName()).isEqualTo("Golikova");
+            Assertions.assertThat(secondPerson.getCountry()).isEqualTo("Russia");
+            Assertions.assertThat(secondPerson.getAge()).isEqualTo(28);
 
             PersonModel thirdPerson = personModel.get(2);
-            assertEquals(3, thirdPerson.getId());
-            assertEquals("Elizabeth", thirdPerson.getFirstName());
-            assertEquals("Golikova", thirdPerson.getLastName());
-            assertEquals("Russia", thirdPerson.getCountry());
-            assertEquals(13, thirdPerson.getAge());
+            Assertions.assertThat(thirdPerson.getId()).isEqualTo(3);
+            Assertions.assertThat(thirdPerson.getFirstName()).isEqualTo("Elizabeth");
+            Assertions.assertThat(thirdPerson.getLastName()).isEqualTo("Golikova");
+            Assertions.assertThat(thirdPerson.getCountry()).isEqualTo("Russia");
+            Assertions.assertThat(thirdPerson.getAge()).isEqualTo(13);
 
         }
     }
